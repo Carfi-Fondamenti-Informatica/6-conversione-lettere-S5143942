2645e79d13efa12bd1511e5d2c708c5640822b02
S5143942 <92802547+S5143942@users.noreply.github.com>
2021-12-01 12:15:38 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,17 +1,12 @@
-#include "lib.h"
-bool conversione (char &a) {
-  bool t=true;
-  if (a>=65 and a<=90 or a>=97 and a<=122){
-    t=true;
-    if (a>=65 and a<=90){
-      a=a+32;
-      return a;
-    } else {
-      a=a-32;
-       return a;
-    }
-  }else{
-    t=false;
-  }
-  return t;
-}
+//
+// Created by S5143942
+//
+
+
+
+#ifndef ESERCITAZIONE_6_LIBR_H
+#define ESERCITAZIONE_6_LIB_H
+
+#endif //ESERCITAZIONE_6_LIB_H
+
+bool conversione (char&);
